a9a05a1a55ba23a2566ff0b04cebaaebcce1cc1f
BbChip <bbchip13@gmail.com>
2017-05-12 21:13:22 +0900

    BinaryTree Update

--- BinaryTreePractice/BinaryTree.h
+++ BinaryTreePractice/BinaryTree.h
@@ -17,9 +17,9 @@ public:
   T GetData();
   void SetData(const T _data);
   Node * GetLeft();
-  void SetLeft(const Node<T> * _data);
+  void SetLeft(Node<T> * const _data);
   Node * GetRight();
-  void SetRight(const Node<T> * _data);
+  void SetRight(Node<T> * const _data);
 };
 
 template <class T>
@@ -30,9 +30,8 @@ Node<T>::Node(){
 
 template <class T>
 Node<T>::Node(T _data){
+  Node();
   this->data = _data;
-  this->p_left = NULL;
-  this->p_right = NULL;
 }
 
 template <class T>
@@ -51,7 +50,7 @@ Node<T> * Node<T>::GetLeft(){
 }
 
 template <class T>
-void Node<T>::SetLeft(const Node<T> * _p_left){
+void Node<T>::SetLeft(Node<T> * const _p_left){
   this->p_left = _p_left;
 }
 
@@ -61,13 +60,14 @@ Node<T> * Node<T>::GetRight(){
 }
 
 template <class T>
-void Node<T>::SetRight(const Node<T> * _p_right){
+void Node<T>::SetRight(Node<T> * const _p_right){
   this->p_right = _p_right;
 }
 
 template <class T>
 class BinaryTree {
 private:
+  Node<T> * root_node;
   Node<T> * current_node;
 
 public:
@@ -79,35 +79,51 @@ public:
   // 루트 노드를 생성하고, 입력된 값을 대입한다.
   // Creates a root node, and set input value
 
+  ~BinaryTree();
+  // 남은 노드를 다 지우고 객체를 삭제한다.
+  // Remove remained node, And remove object
+
   T GetData();
   // 노드에 저장된 데이터를 반환한다.
   // Return stored data in the node.
 
-  void SetData(T _data);
+  void SetData(const T _data);
   // 노드에 데이터를 저장한다. _data로 전달된 값을 저장한다.
   // Stores the data passed to _data in the node.
 
-  Node<T> * GetLeftSubTree();
-  // 왼쪽 서브 트리의 주소 값을 반환한다.
-  // Returns the address of the left subtree.
+  T GetLeftSubTree();
+  // 왼쪽 서브 트리의 값을 반환한다.
+  // Returns the value of the left subtree.
 
   void MakeLeftSubTree();
   // 왼쪽 서브 트리를 연결한다.
   // Connect the left sub tree
 
-  Node<T> * GetRightSubTree();
-  // 오른쪽 서브 트리의 주소 값을 반환한다.
-  // Returns the address of the right subtree.
+  void MakeLeftSubTree(const T _data);
+  // 왼쪽 서브 트리를 연결하고 값을 대입한다.
+  // Connect the left sub tree, And insert value
+
+  T GetRightSubTree();
+  // 오른쪽 서브 트리의 값을 반환한다.
+  // Returns the value of the right subtree.
 
   void MakeRightSubTree();
   // 오른쪽 서브 트리를 연결한다.
   // Connect the right sub tree
 
+  void MakeRightSubTree(const T _data);
+  // 오른쪽 서브 트리를 연결하고 값을 대입한다.
+  // Connect the right sub tree, And insert value
+
+  void Remove();
+
+  void Remove(Node<T> * const _p_node);
+
   void PostorderTraverse();
   // 메모리 누수를 방지하기 위해, 후위 순회를 통해 노드를 삭제한다
   // To prevent memory leak, Use postorder traverse to remove
 
-  void PostorderTraverse(const Node<T> * _p_node);
+  void PostorderTraverse(Node<T> * const _p_node);
   // 메모리 누수를 방지하기 위해, 후위 순회를 통해 노드를 삭제한다
   // To prevent memory leak, Use postorder traverse to remove
 
@@ -118,39 +134,38 @@ private:
 template <class T>
 BinaryTree<T>::BinaryTree(){
   current_node = new Node<T>;
+  root_node = current_node;
 }
 
 template <class T>
-BinaryTree<T>::BinaryTree(const T _value){
+BinaryTree<T>::BinaryTree(const T _data){
   BinaryTree();
-  current_node->SetData(_value);
+  SetData(_data);
+}
+
+template <class T>
+BinaryTree<T>::~BinaryTree(){
+  if(root_node != NULL){
+    Remove(root_node);
+  }
 }
 
 template <class T>
 T BinaryTree<T>::GetData(){
   if(current_node == NULL){
     // error
+    return 0;
   } else {
     return current_node->GetData();
   }
-
 }
 
 template <class T>
-void BinaryTree<T>::SetData(T _data){
+void BinaryTree<T>::SetData(const T _data){
   if(current_node == NULL){
     // error
   } else {
     current_node->SetData(_data);
-  }
-}
-
-template <class T>
-Node<T> * BinaryTree<T>::GetLeftSubTree(){
-  if(current_node == NULL){
-    // error
-  } else {
-    return current_node->GetLeft();
   }
 }
 
@@ -161,24 +176,55 @@ void BinaryTree<T>::MakeLeftSubTree(){
   if(current_node == NULL){
     // error
     return;
+  }else if(current_node->GetLeft() == NULL){
+    p_temp = new Node<T>;
+    current_node->SetLeft(p_temp);
+  }
+  else{
+    Remove(current_node->GetLeft());
+
+    p_temp = new Node<T>;
+    current_node->SetLeft(p_temp);
+  }
+}
+
+template <class T>
+void BinaryTree<T>::MakeLeftSubTree(const T _data){
+  Node<T> * p_temp;
+
+  if(current_node == NULL){
+    // error
+    return;
+  }else if(current_node->GetLeft() == NULL){
+    p_temp = new Node<T>;
+    current_node->SetLeft(p_temp);
+  }
+  else{
+    Remove(current_node->GetLeft());
+
+    p_temp = new Node<T>;
+    current_node->SetLeft(p_temp);
+  }
+
+  p_temp->SetData(_data);
+}
+
+template <class T>
+T BinaryTree<T>::GetLeftSubTree(){
+  Node<T> * p_temp;
+
+  if(current_node == NULL){
+    // error
+    return 0;
   } else {
     p_temp = current_node->GetLeft();
   }
 
   if(p_temp == NULL){
-    p_temp = new Node<T>;
-  }
-  else{
-    //remove first, making node is second
-  }
-}
-
-template <class T>
-Node<T> * BinaryTree<T>::GetRightSubTree(){
-  if(current_node == NULL){
     // error
+    return 0;
   } else {
-    return current_node->GetRight();
+    return p_temp->GetData();
   }
 }
 
@@ -195,10 +241,77 @@ void BinaryTree<T>::MakeRightSubTree(){
 
   if(p_temp == NULL){
     p_temp = new Node<T>;
+    current_node->SetLeft(p_temp);
   }
   else{
-    //remove first, making node is second
+    Remove(current_node->GetRight());
+
+    p_temp = new Node<T>;
+    current_node->SetRight(p_temp);
   }
+}
+
+template <class T>
+void BinaryTree<T>::MakeRightSubTree(const T _data){
+  Node<T> * p_temp;
+
+  if(current_node == NULL){
+    // error
+    return;
+  } else {
+    p_temp = current_node->GetRight();
+  }
+
+  if(p_temp == NULL){
+    p_temp = new Node<T>;
+    current_node->SetLeft(p_temp);
+  }
+  else{
+    Remove(current_node->GetRight());
+
+    p_temp = new Node<T>;
+    current_node->SetRight(p_temp);
+  }
+
+    p_temp->SetData(_data);
+}
+
+template <class T>
+T BinaryTree<T>::GetRightSubTree(){
+  Node<T> * p_temp;
+
+  if(current_node == NULL){
+    // error
+    return 0;
+  } else {
+    p_temp = current_node->GetRight();
+  }
+
+  if(p_temp == NULL){
+    // error
+    return 0;
+  } else {
+    return p_temp->GetData();
+  }
+}
+
+template <class T>
+void BinaryTree<T>::Remove(){
+  Remove(current_node);
+}
+
+template <class T>
+void BinaryTree<T>::Remove(Node<T> * const _p_node){
+  Node<T> * const traverse_node = _p_node;
+
+  if(traverse_node == NULL){
+    return;
+  }
+
+  Remove(traverse_node->GetLeft());
+  Remove(traverse_node->GetRight());
+
+  delete traverse_node;
 }
 
 template <class T>
@@ -207,14 +320,14 @@ void BinaryTree<T>::PostorderTraverse(){
 }
 
 template <class T>
-void BinaryTree<T>::PostorderTraverse(const Node<T> * _p_node){
-  const Node<T> * traverse_node = _p_node;
+void BinaryTree<T>::PostorderTraverse(Node<T> * const _p_node){
+  Node<T> * const traverse_node = _p_node;
 
   if(traverse_node == NULL){
     return;
   }
 
-  //std::cout << GetData() << std::endl;
+  std::cout << traverse_node << std::endl;
 
   PostorderTraverse(traverse_node->GetLeft());
   PostorderTraverse(traverse_node->GetRight());
--- BinaryTreePractice/Test.cpp
+++ BinaryTreePractice/Test.cpp
@@ -2,7 +2,25 @@
 #include "BinaryTree.h"
 
 int main(void){
+  BinaryTree<int> * a = new BinaryTree<int>;
+  BinaryTree<int> b(12);
 
+  std::cout << a->GetData() << std::endl;
+  std::cout << b.GetData() << std::endl;
 
+  a->MakeLeftSubTree(1234);
+  b.MakeLeftSubTree();
 
+  std::cout << a->GetLeftSubTree() << std::endl;
+  std::cout << b.GetLeftSubTree() << std::endl;
+
+  a->MakeLeftSubTree(4567);
+  b.MakeLeftSubTree(6666);
+
+  std::cout << a->GetLeftSubTree() << std::endl;
+  std::cout << b.GetLeftSubTree() << std::endl;
+
+  delete a;
+
+  return 0;
 }
